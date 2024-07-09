a7e831a91360c2f8c5625a19795b795e52ce75eb
AnjneyMishraTesting <anjneymishra21@gmail.com>
2024-07-09 20:16:26 +0530

    Sorting Feature Added

--- controllers/products.js
+++ controllers/products.js
@@ -2,13 +2,24 @@ const Product = require("../models/product")
 
 const getAllProducts = async(req,res)=>{
     const queryObject = {};//if something else written which is not present then this fetch all the products
-    const {company,name,featured} = req.query;
+    const {company,name,featured,sort} = req.query;
 
     if(company) queryObject.company=company;
     if(featured) queryObject.featured=featured;
     if(name) queryObject.name={$regex:name,$options:"i"}//i - caseinsensitive (mogoDB regex)
 
-    const productData = await Product.find(queryObject);
+    //sorting
+    let apiData = Product.find(queryObject);
+    if(sort)
+        {
+            //http://localhost:5000/api/products?sort=name,price
+            //we want .sort(name price) 
+            //.sort(price)  - ascending sort
+            //.sort(-price)  - descending sort
+            let sortfix = sort.replace(","," ");
+            apiData = apiData.sort(sortfix)
+        }
+    const productData = await apiData;
     res.status(200).json({productData});
 }
 
