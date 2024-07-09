3fda26a0123eca16ee13e603f93039d67cae2761
AnjneyMishraTesting <anjneymishra21@gmail.com>
2024-07-09 20:38:15 +0530

    Pagination Feature Added

--- controllers/products.js
+++ controllers/products.js
@@ -8,7 +8,7 @@ const getAllProducts = async (req, res) => {
   if (featured) queryObject.featured = featured;
   if (name) queryObject.name = { $regex: name, $options: "i" }; //i - caseinsensitive (mogoDB regex)
 
-  //sorting
+  //sorting Feature
   let apiData = Product.find(queryObject);
   if (sort) {
     //http://localhost:5000/api/products?sort=name,price
@@ -18,14 +18,24 @@ const getAllProducts = async (req, res) => {
     let sortfix = sort.split(",").join(" ");
     apiData = apiData.sort(sortfix);
   }
+
+  //Select Feature
   if (select) {
     //http://localhost:5000/api/products?select=name,price
     //we want .select(name price)
     let selectfix = select.split(",").join(" ");
     apiData = apiData.select(selectfix);
   }
+
+  //Pagination Feature
+  let page = Number(req.query.page) || 1;
+  let limit = Number(req.query.limit) || 3;
+  let skip = (page-1)*limit; //how much to skip
+
+  apiData = apiData.skip(skip).limit(limit);
+
   const productData = await apiData;
-  res.status(200).json({ productData });
+  res.status(200).json({ productData , nbHits:productData.length});
 };
 
 module.exports = { getAllProducts };
