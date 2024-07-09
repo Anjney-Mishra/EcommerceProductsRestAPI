467d82bb0f491fbe296e979378718f7b8dd258a1
AnjneyMishraTesting <anjneymishra21@gmail.com>
2024-07-09 19:59:06 +0530

    Advance Search Implementation

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,14 @@
 const Product = require("../models/product")
 
 const getAllProducts = async(req,res)=>{
-    const productData = await Product.find({});//get all product from db
+    const queryObject = {};//if something else written which is not present then this fetch all the products
+    const {company,name,featured} = req.query;
+
+    if(company) queryObject.company=company;
+    if(featured) queryObject.featured=featured;
+    if(name) queryObject.name={$regex:name,$options:"i"}//i - caseinsensitive (mogoDB regex)
+
+    const productData = await Product.find(queryObject);
     res.status(200).json({productData});
 }
 
