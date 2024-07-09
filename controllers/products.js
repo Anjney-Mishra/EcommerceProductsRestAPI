a557102b9377cb14fa3a6347194b26ba5b2e0dd6
AnjneyMishraTesting <anjneymishra21@gmail.com>
2024-07-09 19:12:02 +0530

    GetAllProducts

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,8 @@
+const Product = require("../models/product")
+
 const getAllProducts = async(req,res)=>{
-    res.status(200).json({msg:"Hits getAllProducts Route"});
+    const productData = await Product.find({});//get all product from db
+    res.status(200).json({productData});
 }
 
 module.exports = {getAllProducts}
